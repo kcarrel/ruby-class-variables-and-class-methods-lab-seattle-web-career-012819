2f16116d71fa73623b85d30ca0cc15f34c942bf8
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-31 04:59:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,14 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre] ==  
+      if genre_count[genre] == TRUE
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1 
+      end 
+    end
+    genre_count
+  end 
   
   
 end 
